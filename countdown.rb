f539fe2d1abe480a8b5b0e340e313c7ed718696a
Luis Martinez <luis_mmartinez@live.com>
2020-12-26 21:47:33 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,15 +3,18 @@
 def countdown
   number=10
   while number > 0
-  puts "#{number} SECOND(S)!"
-  number -=1
+    puts "#{number} SECOND(S)!"
+    number -=1
+  end
   "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep
   number=10
   while number > 0
-  puts "#{number} SECOND(S)!"
-  sleep(1)
-  number -=1
+    puts "#{number} SECOND(S)!"
+    sleep(1)
+    number -=1
+  end
   "HAPPY NEW YEAR!"
+end
